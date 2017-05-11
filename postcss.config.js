0272d9decd31ef150c3864a74e122c440f3680b8
Marco Fernández Pranno <mfernandezpranno@gmail.com>
2017-05-11 21:16:01 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
         '>1%',
         'last 4 versions',
         'Firefox ESR',
-        'not ie < 9', // React doesn't support IE8 anyway
+        'not ie < 9'
       ]
     })
   ]
